987d4cf79e9790c488d89ce2e6a39e2f834d1f51
yujin1228 <starng11@naver.com>
2023-10-21 17:34:26 +0900

    🎨 Style : 글로벌스타일 파일명 변경 및 폰트재설정

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import HomePage from 'pages/HomePage/HomePage';
 import React from 'react';
 import AppRouter from 'router/AppRouter';
-import GlobalStyle from 'style/Global.style';
+import GlobalStyle from 'style/GlobalStyle';
 
 export default function App() {
   return (
