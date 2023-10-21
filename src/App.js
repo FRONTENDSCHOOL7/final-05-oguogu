cbd8ee61e3ae08169f9156d151ebd29b8651e204
Meow-Chu <chuyeon0314@gmail.com>
2023-10-22 00:04:52 +0900

    📝 Docs      : chatlist와 chatroom 라우트 두줄은 router 폴더 안에 있는 AppRouter.jsx에 넣고, AppRouter.jsx파일에서 chatlist 와 chatroom 라우팅처리 해주신 후 ChatPage(12행) 는 지웠습니다

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import HomePage from 'pages/HomePage/HomePage';
 import React from 'react';
 import AppRouter from 'router/AppRouter';
 import GlobalStyle from 'style/GlobalStyle';
-import ChatPage from 'pages/ChatPage/ChatPage';
 
 export default function App() {
   return (
     <>
       <GlobalStyle />
       <AppRouter />
-      <ChatPage />
     </>
   );
 }
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,4 +1,6 @@
 import HomePage from 'pages/HomePage/HomePage';
+import ChatList from 'pages/ChatPage/ChatList';
+import ChatRoom from 'pages/ChatPage/ChatRoom';
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
@@ -7,6 +9,8 @@ export default function AppRouter() {
     <BrowserRouter>
       <Routes>
         <Route path="/home" element={<HomePage />} />
+        <Route path="/chatlist" element={<ChatList />} />
+        <Route path="/chatroom" element={<ChatRoom />} />
       </Routes>
     </BrowserRouter>
   );
