ade2f1acfeb9bcc64159a756acd46f8a6f891a15
hanhj0516 <hanhj0516@naver.com>
2023-10-27 03:42:07 +0900

    ✨ Feat : 팔로우 목록 페이지 네비게이션 바 추가

--- src/pages/Follow/Follow.jsx
+++ src/pages/Follow/Follow.jsx
@@ -3,6 +3,7 @@ import { FollowBg, UserList, UserListEle } from './Follow.style'
 import Header from 'components/common/header/Header'
 import userImg from 'assets/images/profile_img_tem.jpg'
 import Button from 'components/common/button/Button'
+import NavBar from 'components/common/navbar/NavBar'
 
 export default function Follow() {
   return (
@@ -24,8 +25,8 @@ export default function Follow() {
           <p><span>쿠루미히어로즈</span>좌충우돌 우당탕탕 말썽쟁이 쿠리미네 </p>
           <Button size='xs'text="팔로우" /> 
         </UserListEle>
-
       </UserList>
+      <NavBar></NavBar>
     </FollowBg>
   )
 }
