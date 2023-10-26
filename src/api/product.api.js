5b6f7c35e2a2a24de98541a0981cb8206236da2b
yujin1228 <starng11@naver.com>
2023-10-26 09:35:01 +0900

    ✨ Feat : 상품관련 api 코드 추가

---
+++ src/api/product.api.js
@@ -0,0 +1,31 @@
+import { tokenInstance } from 'api/axiosInstance';
+
+//상품등록
+export const productUploadAPI = async (product) => {
+  const reqData = {
+    product: {
+      itemName: product.itemName,
+      price: product.price,
+      link: product.link,
+      itemImage: product.itemImg,
+    },
+  };
+
+  try {
+    const result = await tokenInstance.post('product', reqData);
+    console.log(result.data);
+  } catch (error) {
+    throw error;
+  }
+};
+
+//상품 리스트
+export const productListAPI = async (accountname) => {
+  console.log(accountname);
+  try {
+    const result = await tokenInstance.get(`product/${accountname}`);
+    console.log(result.data);
+  } catch (error) {
+    throw error;
+  }
+};
