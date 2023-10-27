026b321a25afe4bb07a91799308c0ad5a66b330d
hanhj0516 <hanhj0516@naver.com>
2023-10-27 20:22:38 +0900

    ✨ Feat : search 페이지랑 follow 페이지 분기
    - 링크가 search 페이지면 버튼이 사라지게

--- src/pages/Follow/Follow.jsx
+++ src/pages/Follow/Follow.jsx
@@ -4,27 +4,38 @@ import Header from 'components/common/header/Header'
 import userImg from 'assets/images/profile_img_tem.jpg'
 import Button from 'components/common/button/Button'
 import NavBar from 'components/common/navbar/NavBar'
+import { useLocation } from 'react-router'
 
 export default function Follow() {
+  const location = useLocation();
+  const { pathname } = location;
+
+  const isSearchPath = pathname === '/search';
+  
   return (
     <FollowBg>
-      <Header  type="follow" text="Followings"  />
+      {isSearchPath ? (
+        <Header type="search" />
+      ) : (
+        <Header type="follow" text="Followings" />
+      )}
       <UserList>
         <UserListEle>
           <img src={userImg} alt='유저 프로필'></img>
           <p><span>쿠루미히어로즈</span>좌충우돌 우당탕탕 말썽쟁이 쿠리미네 </p>
-          <Button size='xs'text="팔로우" /> 
+          {isSearchPath ? null : <Button size='xs' text="팔로우" />}
         </UserListEle>
         <UserListEle>
           <img src={userImg} alt='유저 프로필'></img>
           <p><span>쿠루미히어로즈</span>좌충우돌 우당탕탕 말썽쟁이 쿠리미네 </p>
-          <Button size='xs'text="팔로우" /> 
+          {isSearchPath ? null : <Button size='xs' text="팔로우" />}
         </UserListEle>
         <UserListEle>
           <img src={userImg} alt='유저 프로필'></img>
           <p><span>쿠루미히어로즈</span>좌충우돌 우당탕탕 말썽쟁이 쿠리미네 </p>
-          <Button size='xs'text="팔로우" /> 
+          {isSearchPath ? null : <Button size='xs' text="팔로우" />}
         </UserListEle>
+
       </UserList>
       <NavBar></NavBar>
     </FollowBg>
