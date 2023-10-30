fb4c120a8dad77d6c0b0658c97636e62b90fa643
hanhj0516 <hanhj0516@naver.com>
2023-10-30 16:39:03 +0900

    ✨ Feat : 로그아웃 기능 구현
    - 마이페이지에서 모달로 로그아웃 실행시 'oguToken'과 'oguUserInfo'을 지워 로그아웃 실행

--- src/components/common/modal/ModalAlert.jsx
+++ src/components/common/modal/ModalAlert.jsx
@@ -1,10 +1,17 @@
 import React, { useState } from 'react';
+import { useHistory, useNavigate } from 'react-router-dom'; // React Router의 useHistory
 import { BlackBg, ModalAlertBg, ModalAlertNo, ModalAlertYes } from './ModalAlert.style';
 
 export default function ModalAlert({ onClose }) {
-
   const stopPropagation = (e) => {
     e.stopPropagation();
+  };
+  const navigate = useNavigate(); 
+  const handleLogout = () => {
+    console.log('로그아웃이 수행됩니다.');
+    localStorage.removeItem('oguToken');
+    localStorage.removeItem('oguUserInfo');
+    navigate('/');
   };
 
   return (
@@ -13,7 +20,7 @@ export default function ModalAlert({ onClose }) {
         <ModalAlertBg onClick={stopPropagation}>
           <p>로그아웃하시겠어요?</p>
           <ModalAlertNo onClick={onClose}>취소</ModalAlertNo>
-          <ModalAlertYes>로그아웃</ModalAlertYes>
+          <ModalAlertYes onClick={handleLogout}>로그아웃</ModalAlertYes>
         </ModalAlertBg>
       </BlackBg>
     </div>
--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -6,21 +6,29 @@ import { ScrollContainer } from 'components/common/container/Container.style';
 import ProductList from 'components/product/ProductList';
 import Feed from 'components/profile/Feed';
 import { useParams } from 'react-router';
+import ModalBottom from 'components/common/modal/Modalbottom';
 
 export default function ProfilePage() {
   const { accountname } = useParams();
   const userInfo = JSON.parse(localStorage.getItem('oguUserInfo'));
   const isMyProfile = accountname === userInfo.accountname;
 
+  const [ModalBottomOpen, setModalBottomOpen] = useState(false);
+  const toggleModalBottom = () => {
+    setModalBottomOpen(!ModalBottomOpen);
+  };
+
   return (
     <>
-      <Header />
+      <Header rightOnClick={toggleModalBottom}/>
       <ScrollContainer>
         <ProfileBox accountname={accountname} isMyProfile={isMyProfile} />
         <ProductList type="profile" accountname={accountname} />
         <Feed accountname={accountname} />
       </ScrollContainer>
       <NavBar></NavBar>
+      {ModalBottomOpen && 
+      <ModalBottom/>}
     </>
   );
 }
