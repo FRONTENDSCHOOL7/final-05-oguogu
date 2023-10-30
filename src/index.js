bbdb3cc73b7f2bf6f20b49dbe59e80bfcfe6267d
yujin1228 <starng11@naver.com>
2023-10-30 10:55:38 +0900

    🐞 Fix : 하단메뉴탭 수정 및 채팅페이지에 하단메뉴탭 추가
    하단메뉴탭에 메뉴로 존재하는 페이지 외 다른 페이지에서는 메뉴탭이 비활성화되는 이슈 발견
    -> 현재 선택된 메뉴탭을 전역상태로 관리해 모든 페이지에 하단메뉴탭이 마지막으로 선택된 메뉴탭을 공유

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'style/index.css';
 import App from './App';
+import { RecoilRoot } from 'recoil';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <App />
+  <RecoilRoot>
+    <App />
+  </RecoilRoot>
   // <React.StrictMode>
   //   <App />
   // </React.StrictMode>
