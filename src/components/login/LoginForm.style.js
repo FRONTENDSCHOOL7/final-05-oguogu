c6838491e447467f8df17d39ac6e4268b0a086ae
Meow-Chu <chuyeon0314@gmail.com>
2023-12-27 20:45:44 +0900

    💬 Comment   : 일반 리액트 컴포넌트인 Button을 styled-components의 styled 함수 객체로 사용하여 스타일을 적용할 수 없음을 확인하여 질문 주석을 삭제함

--- src/components/login/LoginForm.style.js
+++ src/components/login/LoginForm.style.js
@@ -1,14 +1,8 @@
 import styled from 'styled-components';
-// import Button from 'components/common/button/Button';
 
 export const StyledInputWrap = styled.div`
   position: relative;
 `;
-
-// export const StyledButton = styled(Button)`
-//   margin-top: 30px;
-// `;
-// Button에 margin-top 속성을 추가하면 왜 스타일링이 반영이 안되는거지...?
 
 export const StyledButtonWrap = styled.div`
   margin-top: 30px;
