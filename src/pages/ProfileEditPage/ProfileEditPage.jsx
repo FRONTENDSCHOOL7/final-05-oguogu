de5edaf5e9c98b49d3519d1995973ae1cd155549
Meow-Chu <chuyeon0314@gmail.com>
2024-01-10 16:24:36 +0900

    ♻️ Refactor   : REQ-010 프로필 수정 페이지

--- src/pages/ProfileEditPage/ProfileEditPage.jsx
+++ src/pages/ProfileEditPage/ProfileEditPage.jsx
@@ -1,5 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { myInfoAPI } from 'api/profile.api';
+import ProfileEditForm from 'components/profile-edit/ProfileEditForm';
 
 export default function ProfileEditPage() {
-  return <div>ProfileEditPage</div>;
+  const [userInfo, setUserInfo] = useState({
+    image: '',
+    username: '',
+    accountname: '',
+    intro: '',
+  });
+
+  useEffect(() => {
+    // accountname을 사용하여 프로필 정보 가져오기
+    const prevUserInfo = async () => {
+      try {
+        const storedToken = localStorage.getItem('oguToken');
+        if (!storedToken) {
+          console.log('Token not found. Redirecting to login.');
+          return;
+        }
+        const res = await myInfoAPI(storedToken);
+        const { image, username, accountname, intro } = res;
+        setUserInfo({ image, username, accountname, intro });
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    prevUserInfo();
+  }, []);
+
+  return <ProfileEditForm userInfo={userInfo} />;
 }
